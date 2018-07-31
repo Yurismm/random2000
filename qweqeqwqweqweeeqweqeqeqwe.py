afba94eca0814a6413756cf3ee7d68d3116152f9
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2018-07-31 18:22:53 +0100

    erwr
    erwerwerwe

---
+++ qweqeqwqweqweeeqweqeqeqwe.py
@@ -0,0 +1,29 @@
+import discord
+import asyncio
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content == "gay":
+        await client.send_message(message.channel,"@Welcomer#5491,@Welcomer#5491,@Welcomer#5491,@Welcomer#5491,@Welcomer#5491,@Welcomer#5491,@Welcomer#5491,@Welcomer#5491")
+        await client.send_message(message.channel,"@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491")
+        await client.send_message(message.channel,"@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491")
+        await client.send_message(message.channel,"@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491")
+        await client.send_message(message.channel,"@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491")
+        await client.send_message(message.channel,"@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491")
+    if message.content == "pansexual":
+        await client.send_message(message.channel,"@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491@Welcomer#5491")
+    if message.content == "lesbian":
+        await client.send_message(message.channel,"lesbian...lesbeannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn. No?? okay then :<")
+    if message.content == "asexual":
+        await client.send_message(message.channel,"asexual uh...assexual")
+    if message.content == "qwertyuiopsexual":
+        await client.send_message(message.channel,"Stop, just stop.")
+    if message.content == "demisexual":
+        await client.send_message(message.channel,";-;")
+    if message.content == "ayy":
+        await client.send_message(message.channel,"lmao")
+
+
+
+client.run('NDczODk3ODk4MzgwMjMwNjcx.DkIoBw.G2KWJDWRb81bIwuVIo08MEUk05k')
